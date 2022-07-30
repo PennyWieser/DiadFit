217e2364d3b62c2ed47ad7337d6f6f32c4344df1
Penny Wieser <penny.wieser@gmail.com>
2022-07-30 15:01:42 -0700

    Ability to MonteCarlo around two step density profiles

--- src/DiadFit/error_propagation.py
+++ src/DiadFit/error_propagation.py
@@ -4,57 +4,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import DiadFit as pf
 
-
-def convert_density_depth_Coolprop(T_K=None, CO2_dens_gcm3=None,
-                                   Crust_dens_gcm3=None, output='kbar'):
-
-    try:
-        import CoolProp.CoolProp as cp
-    except ImportError:
-        raise RuntimeError('You havent installed CoolProp, which is required to convert FI densities to pressures. If you have python through conda, run conda install -c conda-forge coolprop in your command line')
-
-
-    if type(T_K) is pd.Series:
-        T_K=T_K.values
-    if type(CO2_dens_gcm3) is pd.Series:
-        CO2_dens_gcm3=CO2_dens_gcm3.values
-
-    density_SI_units=CO2_dens_gcm3*1000
-    P_Pa=cp.PropsSI('P', 'D', density_SI_units, 'T', T_K, 'CO2')
-    P_kbar=P_Pa*10**(-8)
-    if output=='kbar':
-        return P_kbar
-    if output=='MPa':
-        return P_kbar*100
-
-    if output=='df':
-
-        Depth_km=10**(-3)*P_Pa/((Crust_dens_gcm3*1000)*9.81)
-        if type(P_kbar) is float:
-        # Crustal density, using P=rho g H
-            df=pd.DataFrame(data={'Pressure (kbar)': P_kbar,
-                                'Pressure (MPa)': P_kbar*100,
-                                'Depth (km)': Depth_km,
-                                'input_Crust_dens_gcm3': Crust_dens_gcm3,
-                                'input_T_K': T_K,
-                                'input_CO2_dens_gcm3': CO2_dens_gcm3}, index=[0])
-
-        else:
-
-
-            df=pd.DataFrame(data={'Pressure (kbar)': P_kbar,
-                                'Pressure (MPa)': P_kbar*100,
-                                'Depth (km)': Depth_km,
-                                'input_Crust_dens_gcm3': Crust_dens_gcm3,
-                                'input_T_K': T_K,
-                                'input_CO2_dens_gcm3': CO2_dens_gcm3})
-
-
-        return df
+from DiadFit.density_depth_crustal_profiles import *
 
 
 
-def calculate_temperature_density_MC(df=None, sample_i=1, crust_dens_gcm3=2.7, N_dup=1000,
+
+def calculate_temperature_density_MC(sample_i=1,  N_dup=1000, T_K=None, CO2_density_gcm3=None,
+crust_dens_kgm3=None, d1=None, d2=None, rho1=None, rho2=None, rho3=None,
 error_T_K=30, error_type_T_K='Abs', error_dist_T_K='normal',
 error_CO2_dens=0.01, error_type_CO2_dens='Abs', error_dist_CO2_dens='normal',
 error_crust_dens=0.1, error_type_crust_dens='Abs', error_dist_crust_dens='normal',
@@ -99,7 +55,11 @@ plot_figure=True):
 
     """
 
-    df_c=df.copy()
+    #df_c=df.copy()
+    df_c=pd.DataFrame(data={'T_K': T_K,
+                            'CO2_density_gcm3': CO2_density_gcm3
+
+    })
 
     # Temperature error distribution
     if error_type_T_K=='Abs':
@@ -112,7 +72,7 @@ plot_figure=True):
         Noise_to_add_T_K = np.random.uniform(- error_T_K, +
                                                       error_T_K, N_dup)
 
-    T_K_with_noise=Noise_to_add_T_K+df['T_K'].iloc[sample_i]
+    T_K_with_noise=Noise_to_add_T_K+df_c['T_K'].iloc[sample_i]
     T_K_with_noise[T_K_with_noise < 0.0001] = 0.0001
 
 
@@ -121,47 +81,83 @@ plot_figure=True):
     if error_type_CO2_dens=='Abs':
         error_CO2_dens=error_CO2_dens
     if error_type_CO2_dens =='Perc':
-        error_CO2_dens=df_c['CO2_dens'].iloc[sample_i]*error_CO2_dens/100
+        error_CO2_dens=df_c['CO2_density_gcm3'].iloc[sample_i]*error_CO2_dens/100
     if error_dist_CO2_dens=='normal':
         Noise_to_add_CO2_dens = np.random.normal(0, error_CO2_dens, N_dup)
     if error_dist_CO2_dens=='uniform':
         Noise_to_add_CO2_dens = np.random.uniform(- error_CO2_dens, +
                                                       error_CO2_dens, N_dup)
 
-    CO2_dens_with_noise=Noise_to_add_CO2_dens+df_c['Density_g_cm3'].iloc[sample_i]
+    CO2_dens_with_noise=Noise_to_add_CO2_dens+df_c['CO2_density_gcm3'].iloc[sample_i]
     CO2_dens_with_noise[CO2_dens_with_noise < 0.0001] = 0.0001
 
     # Crustal density noise
+    # First need to work out what crustal density is
 
-    if error_type_crust_dens=='Abs':
-        error_crust_dens=error_crust_dens
-    if error_type_crust_dens =='Perc':
-        error_crust_dens=FIs['crust_dens'].iloc[sample_i]*error_crust_dens/100
-    if error_dist_crust_dens=='normal':
-        Noise_to_add_crust_dens = np.random.normal(0, error_crust_dens, N_dup)
-    if error_dist_crust_dens=='uniform':
-        Noise_to_add_crust_dens = np.random.uniform(- error_crust_dens, +
-                                                      error_crust_dens, N_dup)
+    if type(crust_dens_kgm3) is float or type(crust_dens_kgm3) is int:
+        # This is the simplicest scenario, just makes a distribution of pressures
 
-    crust_dens_with_noise=Noise_to_add_crust_dens+crust_dens_gcm3
-    crust_dens_with_noise[crust_dens_with_noise < 0.0001] = 0.0001
+        if error_type_crust_dens=='Abs':
+            error_crust_dens=error_crust_dens
+        if error_type_crust_dens =='Perc':
+            error_crust_dens=crust_dens_kgm3.iloc[sample_i]*error_crust_dens/100
+        if error_dist_crust_dens=='normal':
+            Noise_to_add_crust_dens = np.random.normal(0, error_crust_dens, N_dup)
+        if error_dist_crust_dens=='uniform':
+            Noise_to_add_crust_dens = np.random.uniform(- error_crust_dens, +
+                                                        error_crust_dens, N_dup)
 
-    df_out=pd.DataFrame(data={'T_K_with_noise': T_K_with_noise,
-                              'CO2_dens_with_noise': CO2_dens_with_noise,
-                              'crust_dens_with_noise': crust_dens_with_noise,
-                               'T_K': df['T_K'].iloc[sample_i],
-                              'Density_g_cm3': df['Density_g_cm3'].iloc[sample_i],
-                              'Crustal Density': crust_dens_gcm3,
-                              'error_T_K': error_T_K,
-                              'error_type_T_K': error_type_T_K,
-                              'error_dist_T_K': error_dist_T_K,
-                              'error_CO2_dens': error_CO2_dens,
-                              'error_type_CO2_dens': error_type_CO2_dens,
-                              'error_dist_CO2_dens': error_dist_CO2_dens,
-                              'error_crust_dens': error_crust_dens,
-                              'error_type_crust_dens': error_type_crust_dens,
-                              'error_dist_crust_dens': error_dist_crust_dens,
-                             })
+        crust_dens_with_noise=Noise_to_add_crust_dens+crust_dens_kgm3
+        crust_dens_with_noise[crust_dens_with_noise < 0.0001] = 0.0001
+
+
+        df_out=pd.DataFrame(data={'T_K_with_noise': T_K_with_noise,
+                                'CO2_dens_with_noise': CO2_dens_with_noise,
+                                'crust_dens_with_noise': crust_dens_with_noise,
+                                'T_K': df_c['T_K'].iloc[sample_i],
+                                'CO2_density_gcm3': df_c['CO2_density_gcm3'].iloc[sample_i],
+                                'Crustal Density_kg_m3': crust_dens_kgm3,
+                                'model': None,
+                                'error_T_K': error_T_K,
+                                'error_type_T_K': error_type_T_K,
+                                'error_dist_T_K': error_dist_T_K,
+                                'error_CO2_dens': error_CO2_dens,
+                                'error_type_CO2_dens': error_type_CO2_dens,
+                                'error_dist_CO2_dens': error_dist_CO2_dens,
+                                'error_crust_dens': error_crust_dens,
+                                'error_type_crust_dens': error_type_crust_dens,
+                                'error_dist_crust_dens': error_dist_crust_dens,
+                                })
+
+    # elif crust_dens_kgm3 == "two-step":
+    #     df_out=1
+    #
+    # elif crust_dens_kgm3 == "three-step":
+    #     df_out=1
+
+    else:
+        if error_crust_dens>0:
+            raise Exception('You cannot use a crustal density model with an error in density due to ambiguity about how to apply this (and because errors will be systematic not random). This variable only works for a constant crustal density. Set error_crust_dens=0')
+        # For all other models
+
+
+
+        df_out=pd.DataFrame(data={'T_K_with_noise': T_K_with_noise,
+                                'CO2_dens_with_noise': CO2_dens_with_noise,
+                                'crust_dens_with_noise': crust_dens_kgm3,
+                                'T_K': df_c['T_K'].iloc[sample_i],
+                                'CO2_density_gcm3': df_c['CO2_density_gcm3'].iloc[sample_i],
+                                'Crustal Density_kg_m3': crust_dens_kgm3,
+                                'error_T_K': error_T_K,
+                                'error_type_T_K': error_type_T_K,
+                                'error_dist_T_K': error_dist_T_K,
+                                'error_CO2_dens': error_CO2_dens,
+                                'error_type_CO2_dens': error_type_CO2_dens,
+                                'error_dist_CO2_dens': error_dist_CO2_dens,
+                                'error_crust_dens': error_crust_dens,
+                                'error_type_crust_dens': error_type_crust_dens,
+                                'error_dist_crust_dens': error_dist_crust_dens,
+                                })
 
     if plot_figure is True:
         fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(13,5))
@@ -220,43 +216,56 @@ plot_figure=True):
     return df_out
 
 
-crust_dens_gcm3=2.7
 
-def loop_all_FI_MC(df=None, sample_ID=None, crust_dens_gcm3=2.7, N_dup=1000,
-                error_T_K=30, error_CO2_dens=0.005, error_crust_dens=0.1,
-                error_type_T_K='Abs', error_dist_T_K='normal',
-                error_type_CO2_dens='Abs', error_dist_CO2_dens='normal',
-                error_type_crust_dens='Abs',
-                error_dist_crust_dens='uniform',
+
+def loop_all_FI_MC(df=None, sample_ID=None, N_dup=1000, T_K=None, CO2_density_gcm3=None,
+crust_dens_kgm3=None, d1=None, d2=None, rho1=None, rho2=None, rho3=None,
+error_crust_dens=0.1, error_type_crust_dens='Abs', error_dist_crust_dens='uniform',
+error_T_K=30, error_type_T_K='Abs', error_dist_T_K='normal',
+error_CO2_dens=0.005, error_type_CO2_dens='Abs', error_dist_CO2_dens='normal',
                 plot_figure=False):
 
-    # Set up mean and standard deviation
-    SingleCalc_D_km = np.empty(len(df), dtype=float)
-    SingleCalc_Press_kbar = np.empty(len(df), dtype=float)
+    """ This function propagates errors in CO2 density, temperature and crustal density into pressure and depth distributions
+    """
 
-    mean_Press_kbar = np.empty(len(df), dtype=float)
-    med_Press_kbar = np.empty(len(df), dtype=float)
-    std_Press_kbar = np.empty(len(df), dtype=float)
+    # Set up empty things to fill up.
+    SingleCalc_D_km = np.empty(len(CO2_density_gcm3), dtype=float)
+    SingleCalc_Press_kbar = np.empty(len(CO2_density_gcm3), dtype=float)
 
-    mean_D_km = np.empty(len(df), dtype=float)
-    med_D_km = np.empty(len(df), dtype=float)
-    std_D_km = np.empty(len(df), dtype=float)
-    Sample=np.empty(len(df),  dtype=np.dtype('U100') )
+    mean_Press_kbar = np.empty(len(CO2_density_gcm3), dtype=float)
+    med_Press_kbar = np.empty(len(CO2_density_gcm3), dtype=float)
+    std_Press_kbar = np.empty(len(CO2_density_gcm3), dtype=float)
 
-
-
-
-
-    for i in range(0, len(df)):
+    mean_D_km = np.empty(len(CO2_density_gcm3), dtype=float)
+    med_D_km = np.empty(len(CO2_density_gcm3), dtype=float)
+    std_D_km = np.empty(len(CO2_density_gcm3), dtype=float)
+    Sample=np.empty(len(CO2_density_gcm3),  dtype=np.dtype('U100') )
 
 
 
-        # Working out the errors for each loop
 
+    #This loops through each fluid inclusion
+    for i in range(0, len(CO2_density_gcm3)):
+        if i % 20 == 0:
+            print('working on sample number '+str(i))
+
+        # If user has entered a pandas series for error, takes right one for each loop
         if type(error_T_K) is pd.Series:
             error_T_K=error_T_K.iloc[i]
         else:
             error_T_K=error_T_K
+
+        if type(T_K) is pd.Series:
+            T_K_i=T_K.iloc[i]
+        else:
+            T_K_i=T_K
+
+
+        if type(CO2_density_gcm3) is pd.Series:
+            CO2_density_gcm3_i=CO2_density_gcm3.iloc[i]
+        else:
+            CO2_density_gcm3_i=CO2_density_gcm3
+
 
         if type(error_CO2_dens) is pd.Series:
             error_CO2_dens=error_CO2_dens.iloc[i]
@@ -269,35 +278,36 @@ def loop_all_FI_MC(df=None, sample_ID=None, crust_dens_gcm3=2.7, N_dup=1000,
             error_crust_dens=error_crust_dens
 
 
-        # Make synthetic things
-
-        df_synthetic=calculate_temperature_density_MC(df=df, sample_i=i,
-        crust_dens_gcm3=crust_dens_gcm3, N_dup=N_dup,
-    error_T_K=error_T_K, error_type_T_K=error_type_T_K, error_dist_T_K=error_dist_T_K,
-    error_CO2_dens=error_CO2_dens, error_type_CO2_dens=error_type_CO2_dens, error_dist_CO2_dens=error_dist_CO2_dens,
-    error_crust_dens=error_crust_dens, error_type_crust_dens= error_type_crust_dens,
-    error_dist_crust_dens=error_dist_crust_dens, plot_figure=plot_figure)
+        # This is the function doing the work to actually make the simulations for each variable.
+        df_synthetic=calculate_temperature_density_MC(sample_i=i, N_dup=N_dup, CO2_density_gcm3=CO2_density_gcm3,
+        T_K=T_K, error_T_K=error_T_K, error_type_T_K=error_type_T_K, error_dist_T_K=error_dist_T_K,
+        error_CO2_dens=error_CO2_dens, error_type_CO2_dens=error_type_CO2_dens, error_dist_CO2_dens=error_dist_CO2_dens,
+        crust_dens_kgm3=crust_dens_kgm3,  error_crust_dens=error_crust_dens, error_type_crust_dens= error_type_crust_dens, error_dist_crust_dens=error_dist_crust_dens,
+        d1=d1, d2=d2, rho1=rho1, rho2=rho2, rho3=rho3,
+     plot_figure=plot_figure)
 
         # Convert to densities for MC
 
-        MC_T=convert_density_depth_Coolprop(T_K=df_synthetic['T_K_with_noise'],
+
+        MC_T=convert_co2_density_depth_Coolprop(T_K=df_synthetic['T_K_with_noise'],
                                         CO2_dens_gcm3=df_synthetic['CO2_dens_with_noise'],
-                                       Crust_dens_gcm3=df_synthetic['crust_dens_with_noise'],
+                                       crust_dens_kgm3=df_synthetic['crust_dens_with_noise'],
+                                       d1=d1, d2=d2, rho1=rho1, rho2=rho2, rho3=rho3,
                                          output='df')
 
 
 
         # Singular density calculation
 
-        Densities=convert_density_depth_Coolprop(T_K=df['T_K'].iloc[i],
-                                       Crust_dens_gcm3=crust_dens_gcm3,
-                    CO2_dens_gcm3=df['Density_g_cm3'].iloc[i], output='df')
+        Densities=convert_co2_density_depth_Coolprop(T_K=T_K_i,
+                                       crust_dens_kgm3=crust_dens_kgm3,
+                    CO2_dens_gcm3=CO2_density_gcm3_i, output='df', d1=d1, d2=d2, rho1=rho1, rho2=rho2, rho3=rho3)
 
         # Check of
         if sample_ID is None:
             Sample[i]=i
         else:
-            Sample[i]=df[sample_ID].iloc[i]
+            Sample[i]=sample_ID.iloc[i]
         SingleCalc_D_km[i]=Densities['Depth (km)']
         SingleCalc_Press_kbar[i]=Densities['Pressure (kbar)']
 
