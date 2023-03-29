1e30ed0905dece0336819d13459fa6ed1b0c8790
Penny Wieser <penny.wieser@gmail.com>
2023-03-29 10:56:51 -0700

    spf to pf in unittests

--- Unit_tests/test_DiadFit.py
+++ Unit_tests/test_DiadFit.py
@@ -6,7 +6,7 @@ import DiadFit as pf
 decimalPlace=4
 class test_SP94_EOS(unittest.TestCase):
     def test_SP94_pressure_to_density(self):
-        self.assertAlmostEqual(spf.calculate_rho_for_P_T(P_kbar=1, 
+        self.assertAlmostEqual(pf.calculate_rho_for_P_T(P_kbar=1, 
 T_K=1400, EOS='SW96')[0], 0.300608,
 decimalPlace, "Calculated SP94 P doesnt match test value")
         
