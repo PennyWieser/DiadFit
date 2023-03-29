bb9ff460c5eaa55c96d6a6c83aeb3be1de637c34
Penny Wieser <penny.wieser@gmail.com>
2023-03-29 11:07:17 -0700

    changed to SP duh

--- Unit_tests/test_DiadFit.py
+++ Unit_tests/test_DiadFit.py
@@ -7,7 +7,7 @@ decimalPlace=4
 class test_SP94_EOS(unittest.TestCase):
     def test_SP94_pressure_to_density(self):
         self.assertAlmostEqual(pf.calculate_rho_for_P_T(P_kbar=1, 
-T_K=1400, EOS='SW96')[0], 0.300608,
+T_K=1400, EOS='SP94')[0], 0.297958,
 decimalPlace, "Calculated SP94 P doesnt match test value")
         
 
